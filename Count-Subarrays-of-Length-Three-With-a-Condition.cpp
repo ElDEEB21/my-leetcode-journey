7cd8618e7f872747f478a4fb63e3626a8ad70b9f
Abd El-Rahman Eldeeb <116762512+ElDEEB21@users.noreply.github.com>
2025-04-27 04:32:16 +0300

    [3392] [Time Beats: 100.00%] [Memory Beats: 45.56%] - LeetPush

---
+++ Count-Subarrays-of-Length-Three-With-a-Condition.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int count = 0;
+        for (int i = 1; i < nums.size() - 1; ++i) 
+            count += (nums[i - 1] + nums[i + 1] == nums[i] / 2.0);
+        return count;
+    }
+};
