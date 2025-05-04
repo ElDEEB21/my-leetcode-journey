837d61efd305149b767acb02e912ee11de269585
Abd El-Rahman Eldeeb <116762512+ElDEEB21@users.noreply.github.com>
2025-05-04 04:29:25 +0300

    [1128] [Time Beats: 58.50%] [Memory Beats: 74.00%] - LeetPush

--- Number-of-Equivalent-Domino-Pairs.cpp
+++ Number-of-Equivalent-Domino-Pairs.cpp
@@ -7,9 +7,9 @@ public:
         int result = 0;
         for (auto &domino : dominoes)
         {
-            int key = min(domino[0], domino[1]) * 10 + max(domino[0], domino[1]);
-            result += count[key];
-            count[key]++;
+            int key = (domino[0] < domino[1]) ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+            result += count[key]; 
+            count[key]++;         
         }
         return result;
     }
