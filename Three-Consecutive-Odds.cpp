c3c4f980df8c9d47f947e366e414e119be15bd32
Abd El-Rahman Eldeeb <116762512+ElDEEB21@users.noreply.github.com>
2025-05-11 03:15:29 +0300

    [1550] [Time Beats: 100.00%] [Memory Beats: 59.60%] - LeetPush

---
+++ Three-Consecutive-Odds.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            (arr[i] & 1) ? count++ : count = 0;
+            if (count == 3)
+                return true;
+        }
+        return false;
+    }
+};
